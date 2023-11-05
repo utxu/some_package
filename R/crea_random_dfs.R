7ca0306c69ed0eb75b3e8fdefafcf12891dc8680
utxu <alvzotegui@gmail.com>
2023-11-05 23:37:11 +0100

    añadiendo algo de doc

--- R/crea_random_dfs.R
+++ R/crea_random_dfs.R
@@ -1,5 +1,15 @@
 
-
+#' Crea dataframes con columnas de números aleatorios
+#'
+#' @param n_dfs Número de dataframes finales que queremos tener
+#' @param n_col Número de columnas que ha de tener el df
+#' @param n_rows Número de filas que ha de tener el df
+#' @param interval Intervalo máximo y mínimo de los números aleatorios que rellenan el df
+#'
+#' @return Uno o más dfs
+#' @export
+#'
+#'
 crea_random_dfs <- function(n_dfs, n_col, n_rows, interval) {
   set.seed(1010)
   for (i in 1:n_dfs) {
